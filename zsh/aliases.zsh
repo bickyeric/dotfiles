84baed61d269b627e4ab08608ab3bcfcea1d2672
Bicky Eric Kantona <bickyeric@proton.me>
2025-11-12 10:40:43 +0700

    use docker for alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,23 +1,20 @@
 # ARGOCD CONFIG
 alias argo="argocd"
 
-# PODMAN
-alias docker="podman"
-
-# CLI in PODMAN
-alias kcat="podman run --rm --network host --platform linux/amd64 edenhill/kcat:1.7.1"
-alias mongo="podman run -it --rm --network host mongo:6.0 mongosh"
-alias grpcurl="podman run --rm -it --network host fullstorydev/grpcurl:v1.8.9"
+# CLI in DOCKER
+alias kcat="docker run --rm --network host --platform linux/amd64 edenhill/kcat:1.7.1"
+alias mongo="docker run -it --rm --network host mongo:6.0 mongosh"
+alias grpcurl="docker run --rm -it --network host fullstorydev/grpcurl:v1.8.9"
 
 # POSTGRES CLIENT
 psql() {
-  podman run --rm -it --network host --env PGHOST=127.0.0.1 postgres:17 psql $@
+  docker run --rm -it --network host --env PGHOST=127.0.0.1 postgres:17 psql $@
 }
 
 # MYSQL CLIENT
 mysql() {
-  podman run --rm -it --network host --env MYSQL_HOST=127.0.0.1 mysql:8.0 mysql $@
+  docker run --rm -it --network host --env MYSQL_HOST=127.0.0.1 mysql:8.0 mysql $@
 }
 mysqldump() {
-  podman run --rm -it --network host --env MYSQL_HOST=127.0.0.1 mysql:8.0 mysqldump $@
+  docker run --rm -it --network host --env MYSQL_HOST=127.0.0.1 mysql:8.0 mysqldump $@
 }
